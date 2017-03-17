1aaf8df41ae1849efc8957c8aff1dd72e7df657e
Daniel Rosenberg <drosen@google.com>
2017-03-16 19:33:35 -0700

    ANDROID: sdcardfs: Fix style issues with comments
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 35331000
    Change-Id: I8791ef7eac527645ecb9407908e7e5ece35b8f80

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -127,12 +127,10 @@ static int sdcardfs_hash_ci(const struct dentry *dentry,
 	unsigned long hash;
 
 	name = qstr->name;
-
 	len = qstr->len;
 
 	hash = init_name_hash();
 	while (len--)
-
 		hash = partial_name_hash(tolower(*name++), hash);
 	qstr->hash = end_name_hash(hash);
 
@@ -146,10 +144,7 @@ static int sdcardfs_cmp_ci(const struct dentry *parent,
 		const struct dentry *dentry,
 		unsigned int len, const char *str, const struct qstr *name)
 {
-	/* This function is copy of vfat_cmpi */
-
-
-
+	/* FIXME Should we support national language? */
 
 	if (name->len == len) {
 		if (str_n_case_eq(name->name, str, len))
